58b284a9fc16349e5edd693cef471d5ea2126b30
QuanLySinhVien1111 <tbya8667@gmail.com>
2026-09-23 01:43:29 +0700

    Update LAB 03

--- src/main/java/com/example/quanlysinhvien/controller/PageController.java
+++ src/main/java/com/example/quanlysinhvien/controller/PageController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/student.html";
+    }
+
     @GetMapping("/students")
     public String viewStudentsPage() {
-        return "students";
+        return "redirect:/student.html";
     }
 }
